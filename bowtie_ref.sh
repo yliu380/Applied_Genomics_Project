aa17d2477f31e580ed52a280e6e2036551441ecd
Cindy Ren <xren15@jhu.edu>
2023-11-11 20:36:56 -0500

    initialized the repo

--- bowtie_ref.sh
+++ bowtie_ref.sh
@@ -9,7 +9,7 @@ echo "Building BOWTIE2 index for the reference genome..."
 $BOWTIE2_PATH-build $REFERENCE_GENOME reference_index
 
 # Set the path to the input reads (replace with your actual input file)
-READS="rna_seq/FC305PAHM_20090604_1_1_filtered_query.fasta"
+READS="/Users/aliceliu/Desktop/comp_genomics_schatz/Project/FC305PAHM_20090604_1_1_filtered_query.fasta"
 
 # Set the path for the output SAM file
 OUTPUT_SAM="aligned_reads.sam"
@@ -22,4 +22,3 @@ echo "Alignment completed. Output stored in $OUTPUT_SAM"
 
 #Get alignment quality from sam file in column 5
 awk -F'\t' 'BEGIN {sum=0; count=0} {if($1 !~ /^@/){sum+=$5; count++}} END {print "Sum of MAPQ: " sum; print "Number of Mapped Reads: " count}' aligned_reads.sam > alignment_summary.txt
-
