ce2d11cf5bf2cf4529ac4c6dfbf3db6eee0d7722
Cindy Ren <xren15@jhu.edu>
2023-12-16 01:45:23 -0500

    modified hisat2 script

--- hisat_ref_2.sh
+++ hisat_ref_2.sh
@@ -2,22 +2,26 @@
 
 # Check if exactly three arguments are provided
 if [ $# -ne 3 ]; then
-    echo "Usage: $0 <annotation.gtf> <reads.fastq>"
+    echo "Usage: $0 <reference_genome.fna> <annotation.gtf> <reads.fastq>"
     exit 1
 fi
 
-ANNOTATION=$1
-SPLICE="ref_genome/splice.ss"
-EXON="ref_genome/exon.exon"
+HISAT2_PATH=""
+
+REF_GENOME=$1
+ANNOTATION=$2
+READS=$3
+SPLICE="$(dirname "$ANNOTATION")/splice.ss"
+EXON="$(dirname "$ANNOTATION")/exon.exon"
+REF_PREFIX="$(dirname "$REF_GENOME")/$(basename "$REF_GENOME" .fna)"
+READS_FILENAME=$(basename "$READS" | cut -f1 -d'.')
 
 python3 hisat2_extract_splice_sites.py $ANNOTATION > $SPLICE
 python3 hisat2_extract_exons.py $ANNOTATION > $EXON
 
-./hisat2-build --ss /Users/aliceliu/Desktop/comp_genomics_schatz/Project/splice.ss \
-               --exon /Users/aliceliu/Desktop/comp_genomics_schatz/Project/exon.exon \
-               /Users/aliceliu/Desktop/comp_genomics_schatz/Project/ref_genome/merged_NA12878_maternal.fa \
-               /Users/aliceliu/Desktop/comp_genomics_schatz/Project/hisat/NA12878_maternal_hisat2
+./hisat2-build --ss $SPLICE --exon $EXON $REF_GENOME $REF_PREFIX
 
-time ./hisat2 -x /Users/aliceliu/Desktop/comp_genomics_schatz/Project/hisat/NA12878_maternal_hisat2 \
-              -U /Users/aliceliu/Desktop/comp_genomics_schatz/Project/CaltechEncode_Gm12878_RnaSeq_Rep1.fastq \
-              -S /Users/aliceliu/Desktop/comp_genomics_schatz/Project/hisat/aligned_reads_hisat2_NA12878.sam
+OUTPUT_SAM="hisat/${READS_FILENAME}/aligned_reads.sam"
+
+mkdir -p hisat/$READS_FILENAME
+time ./hisat2 -x $REF_PREFIX -U $READS -S $OUPUT_SAM
