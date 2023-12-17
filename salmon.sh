0da6646ef75ab8bfc744c68edc1cf6130a69ee6b
Cindy Ren <xren15@jhu.edu>
2023-12-16 22:24:12 -0500

    added salmon script

--- salmon.sh
+++ salmon.sh
@@ -1,5 +1,32 @@
-gffread -w /data/mschatz1/yliu_proj/enc004transcripts.fa -g /data/mschatz1/yliu_proj/enc004.hap1.fa /data/mschatz1/yliu_proj/enc004_annotation
+#!/bin/bash
 
-time salmon index -t enc004transcripts.fa -i enc004_salmon_index
+# Check if exactly three arguments are provided
+if [ $# -ne 4 ]; then
+    echo "Usage: $0 <reference_genome.fna> <annotation.gtf> <reads1.fastq> <reads2.fastq>"
+    exit 1
+fi
 
-time salmon quant -i enc004_salmon_index -l A -1 ENCFF145QCK.fastq -2 ENCFF423RWS.fastq --validateMappings -o /data/mschatz1/yliu_proj/salmon
+reference=$1
+annotation=$2
+reads1=$3
+reads2=$4
+
+salmon_path="/home/xren15/mambaforge/bin/salmon"
+
+ref_prefix=$(basename "$reference" .fna)
+working_dir="/data/mschatz1/xren15/salmon/${ref_prefix}"
+
+mkdir -p $working_dir
+gffread -w "${working_dir}/${ref_prefix}.fa" -g $reference $annotation
+
+time $salmon_path index -t "${working_dir}/${ref_prefix}.fa" -i "${working_dir}/${ref_prefix}_index"
+
+if [ ! -e "/data/mschatz1/xren15/star/Genome" ]; then
+  # If not, build the STAR index for the reference genome
+  echo "Building STAR index for the reference genome..."
+  mkdir -p salmon
+  time $salmon_path quant -i "${working_dir}/${ref_prefix}_index" -l A -1 $reads1 -2 $reads2 --validateMappings -o salmon/
+  echo "Index built successfully."
+else
+  echo "STAR index already exists."
+fi
