d3f5114f2f077b7c084ccfb61658c2a5b64c2c2e
Cindy Ren <xren15@jhu.edu>
2023-12-16 22:48:05 -0500

    resolved git conflict

--- salmon.sh
+++ salmon.sh
@@ -17,16 +17,23 @@ ref_prefix=$(basename "$reference" .fna)
 working_dir="/data/mschatz1/xren15/salmon/${ref_prefix}"
 
 mkdir -p $working_dir
+echo "Start processing reference genome based on annotation"
 gffread -w "${working_dir}/${ref_prefix}.fa" -g $reference $annotation
+echo "Reference processing done. Output saved as ${working_dir}/${ref_prefix}.fa."
 
 time $salmon_path index -t "${working_dir}/${ref_prefix}.fa" -i "${working_dir}/${ref_prefix}_index"
 
-if [ ! -e "/data/mschatz1/xren15/salmon/" ]; then
+if [ -z "$(ls -A $working_dir)" ]; then
   # If not, build the STAR index for the reference genome
-  echo "Building STAR index for the reference genome..."
-  mkdir -p salmon
+  echo "Building Salmon index for the reference genome..."
+  mkdir -p $working_dir
   time $salmon_path quant -i "${working_dir}/${ref_prefix}_index" -l A -1 $reads1 -2 $reads2 --validateMappings -o salmon/
   echo "Index built successfully."
 else
-  echo "STAR index already exists."
+  echo "Salmon index already exists."
 fi
+
+echo "Start aligning reads"
+mkdir -p salmon
+time $salmon_path quant -i "${working_dir}/${ref_prefix}_index" -l A -1 $reads1 -2 $reads2 --validateMappings -o salmon/
+echo "Reads aligned successfully. Output stored in ./salmon."
